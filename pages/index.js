ee5390302a7c3feacec69f5e1e928d7696942cd6
pavelsavara <pavel.savara@gmail.com>
2021-12-29 20:06:27 +0100

    znaky

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,21 @@ export default function Home() {
   const screen = new Screen();
 
   for (let s = 0; s < 80; s = s + 1) {
-    screen.writeAt(0, s, '+');
-    screen.writeAt(24, s, '+');
+    screen.writeAt(0, s, '═');
+    screen.writeAt(24, s, '═');
   }
 
   for (let r = 0; r < 25; r = r + 1) {
-    screen.writeAt(r, 0, '+');
-    screen.writeAt(r, 79, '+');
+    screen.writeAt(r, 0, '║');
+    screen.writeAt(r, 79, '║');
   }
+  screen.writeAt(0, 0, '╔');
+  screen.writeAt(0, 79, '╗');
+  screen.writeAt(24, 0, '╚');
+  screen.writeAt(24, 79, '╝');
+  screen.writeAt(0, 1, ' ');
+  screen.writeAt(24, 78, ' ');
+  screen.writeAt(12, 40, '╬');
 
   return (
     <div className={styles.container}>
